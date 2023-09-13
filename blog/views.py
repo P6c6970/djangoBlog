26965df1fe7ed2df9185cdd45d5c2dd7f1050425
P6c6970 <lipaikin.pavel@yandex.ru>
2023-09-13 22:24:54 +0300

    Added sections "liked" and "for you"

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,60 @@ from django.db.models.functions import Coalesce
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
 
 from utils.for_account import login_check
 from .models import Article, LikeArticle
 
 
 def home_page(request):
-    articles = Article.objects.filter(status=True).annotate(likes=Coalesce(Sum("likearticle__like_or_dislike"), 0)).order_by("-likes", )
-    return render(request, 'home.html', {'articles': articles})
+    articles = Article.objects.filter(status=True).annotate(
+        likes=Coalesce(Sum("likearticle__like_or_dislike"), 0)).order_by("-likes", )
+    return render(request, 'articles_list.html', {'title': "Популярное", 'articles': articles})
 
 
 # @login_check()
 def articles_list(request):
     # articles = Article.objects.filter(status=True)
     articles = Article.objects.filter(status=True).annotate(likes=Coalesce(Sum("likearticle__like_or_dislike"), 0))
+    return render(request, 'articles_list.html', {'title': "Последние обновления", 'articles': articles})
+
+
+@login_check()
+def articles_liked_list(request):
+    articles = Article.objects.filter(status=True, likearticle__like_or_dislike=1,
+                                      likearticle__author=request.user).annotate(
+                                            likes=Coalesce(Sum("likearticle__like_or_dislike"), 0))
+    return render(request, 'articles_list.html', {'title': "Понравившиеся", 'null_articles': "У вас еще нет понравившихся статей", 'articles': articles})
+
+
+@method_decorator(login_check(), name='dispatch')
+class ArticleForYou(ListView):
+    """
+    Представление, выводящее список статей авторов, на которые подписан текущий пользователь
+    """
+    model = Article
+    template_name = 'articles_list.html'
+    context_object_name = 'articles'
+    paginate_by = 10
+
+    def get_queryset(self):
+        authors = self.request.user.following.values_list('id', flat=True)
+        queryset = self.model.objects.all().filter(author__id__in=authors).annotate(likes=Coalesce(Sum("likearticle__like_or_dislike"), 0))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Статьи ваших авторов'
+        context['null_articles'] = "У вас еще нет статей этой подборки, подписывайтесь на авторов чтобы исправить это"
+        return context
+
+
+@login_check()
+def articles_for_you_list(request):
+    articles = Article.objects.filter(status=True, likearticle__author__in=request.user.followers).annotate(
+                                            likes=Coalesce(Sum("likearticle__like_or_dislike"), 0))
     return render(request, 'articles_list.html', {'articles': articles})
 
 
@@ -43,6 +83,7 @@ def article_detail(request, article_slug):
 
 @login_check()
 def article_like_or_dislike(request, article_slug):
+    """ajax метод для лайков и дизлайков статей"""
     # request.is_ajax() удалили, поэтому request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
     if request.method == "POST" and request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
         like_or_dislike = int(request.POST["like_or_dislike"])
